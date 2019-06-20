90e8008fdfd72edacd66072aa3ac869808c878f7
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-20 22:31:51 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.each do |lang, types|
+      if
+        new_hash[lang][:style].push(style)
+      else
+        types.each do |type, value|
+          if type == :type
+            new_hash[lang] = {type: value, style: [style]}
+          end
+        end
+      end
+    end
+  end
+  return new_hash
 end
